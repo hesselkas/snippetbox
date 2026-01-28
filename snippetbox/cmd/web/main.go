187a4bd43fe28cd1c86bf10ffdb54cc15b4eb703
hessel <hesss.kas@gmail.com>
2026-01-28 17:02:10 +0100

    wat changes

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
+
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("starting server on :4000")
+	fileServer := http.FileServer(http.Dir("./cmd/web/ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	logger.Info("starting server", "addr", *addr)
+
+	log.Printf("starting server on %s", *addr)
+
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
